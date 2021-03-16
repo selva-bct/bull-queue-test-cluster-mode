493bd6fe11dc74f2ee51e0ceadd029f4ed3c2a57
selva-bct <selvanathan.v@bahwancybertek.com>
2021-03-16 21:34:33 +0000

    Tried different variation of cluster with auth, cluster without auth, nocluster with auth, no cluster no auth

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const BullQueue = require("bull");
+const Redis = require("ioredis");
+const redisClient = require('redis')
 
 const RedisOpts = {
-  host: "clustercfg.testing-bull-queue.orbyno.aps1.cache.amazonaws.com",
-  port: 6379,
-  password: "1234567890abcdef",
+     host:  "clustercfg.bull-q-23-1-cluster-auth.orbyno.aps1.cache.amazonaws.com", // cluster with auth
+  //  host: "bull-q-23-1-cluster-without-auth.orbyno.clustercfg.aps1.cache.amazonaws.com", // cluster with out auth
+      host: "master.no-cluster.orbyno.aps1.cache.amazonaws.com", // no cluster with auth
+//	host: "no-cluster-no-pass.orbyno.ng.0001.aps1.cache.amazonaws.com", //   no cluster with no auth
+    port: 6379,
+  password: "hudsonalphabct123456789",
 };
 
 const getBullOpts = (queueName) => {
@@ -16,10 +21,30 @@ const getBullOpts = (queueName) => {
 const createQueue = (queueName) => {
   return new Promise((resolve, reject) => {
     const bullOpts = getBullOpts(queueName);
+//	  bullOpts.creatClient = (type, config) => new redisClient.createClient(config.port, config.host, { auth_pass: config.password, tls: { servername: config.host } })
+		  
+    bullOpts.createClient = (type, config) => new Redis.Cluster(
+	    [
+		    {
+			    host: config.host,
+			    port: config.port
+		    },
+            ],
+	    {
+		    // slotsRefreshTimeout: 2000,
+		    dnsLookup: (address, callback) => callback(null, address),
+		    redisOptions: {
+		          tls: {},
+		          password: config.password,
+                    }
+	    }
+    )
     const queue = new BullQueue(queueName, bullOpts);
     queue.resume().then(() => {
       resolve(queue);
-    });
+    }).catch(error => {
+      reject(error)
+    })
   });
 };
 
